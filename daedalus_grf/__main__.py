b9514b8626dc12fad952d86962501470da7ec489
xaratustrah <xaratustrah@users.noreply.github.com>
2025-02-04 11:30:24 +0100

    update

--- daedalus_grf/__main__.py
+++ daedalus_grf/__main__.py
@@ -110,8 +110,8 @@ def process_jsons(json1, json2):
         
         name2 = json2['sourceInfo']['deviceName']
         value2 = json2['data']['pressure'] / 100, # convert Pa to mbar
-        if isinstance(value1, tuple):
-            value1 = value1[0]
+        if isinstance(value2, tuple):
+            value2 = value1[0]
         epoch_time2 = int(json2['data']['timestampAcq'] / 1_000_000_000)
                 
     except(KeyError):
